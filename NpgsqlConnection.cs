7e3dece4baa389c3cdef532fca607829e6587f13
Luagushechk <kristinabulatkina04@gmail.com>
2023-10-22 15:02:38 +0800

    Подключение базы данных PostgreSQL

---
+++ NpgsqlConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace Calculator_foormul
+{
+    public class NpgsqlConnection
+    {
+        public string ConnectionString { get; set; }
+
+        public NpgsqlConnection(string connectionString)
+        {
+            ConnectionString = "Host=localhost;Port=5432;Database=PostgreSQL;Username=postgresql;Password=12345111;";
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        }
+    }
+}
